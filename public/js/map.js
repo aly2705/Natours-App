deac8b7f334ffbced487b66a5d3a818ecdb0741b
aly2705 <alexandraaly.simion@gmail.com>
2023-02-19 19:27:17 +0200

    fixed stripe import and leaflet map

--- public/js/map.js
+++ public/js/map.js
@@ -4,14 +4,11 @@ import L from 'leaflet';
 export const displayMap = (locations) => {
   const map = L.map('map', { zoomControl: false });
 
-  L.tileLayer(
-    'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
-    {
-      attribution:
-        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-      crossOrigin: '',
-    }
-  ).addTo(map);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    crossOrigin: '',
+  }).addTo(map);
 
   let points = [];
   locations.forEach((location) => {
